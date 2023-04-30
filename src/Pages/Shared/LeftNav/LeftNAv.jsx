47bab5a06a12f5c897b776474d2d16ee2aad6e93
Suvrodev Howlader <suvrodev.cse@yahoo.com>
2023-04-30 22:32:49 +0600

    added Left Nav

--- src/Pages/Shared/LeftNav/LeftNAv.jsx
+++ src/Pages/Shared/LeftNav/LeftNAv.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNAv = () => {
+    const [categories,setCategories]=useState([])
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/categories')
+        .then(res=>res.json())
+        .then(data=>setCategories(data))
+    },[])
     return (
         <div>
-            <h4>Left Navigation</h4>
+            <h4>All Categories</h4>
+            <div className='mt-4 text-start ps-3'>
+                {
+                    categories.map(category=> <p key={category.id}>
+                        <Link className='fw-bold text-secondary' to={`/category/${category.id}`}> {category.name} </Link>
+                        </p>
+                        )
+                }
+            </div>
         </div>
     );
 };
