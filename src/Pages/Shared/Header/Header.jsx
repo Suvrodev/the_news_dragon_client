c963299797d6d29df7f71b6dca0b8f45d1e34ca7
Suvrodev Howlader <suvrodev.cse@yahoo.com>
2023-04-30 20:31:28 +0600

    Added Header

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import headerLogo from '../../../assets/logo.png'
+import moment from 'moment/moment';
 const Header = () => {
     return (
-        <div>
-            <h1>This is Header</h1>
+        <div className='text-center'>
+            <img src={headerLogo} alt="" />
+            <p className='text-secondary' ><small>Journalism Without Fear or Favor</small></p>
+            <p className='text-success fw-bold'> {moment().format('dddd, D MMMM  YYYY')} </p>
         </div>
     );
 };
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -4,26 +4,28 @@ import Footer from "../Pages/Shared/Footer/Footer";
 import { Col, Container, Row } from "react-bootstrap";
 import LeftNAv from "../Pages/Shared/LeftNav/LeftNAv";
 import RightNav from "../Pages/Shared/RightNav/RightNav";
+import Header from "../Pages/Shared/Header/Header";
 
 const Main = () => {
   return (
     <div>
-      <h1>This is main</h1>
-      <Container>
-        <Row>
-          <Col lg>
-             <LeftNAv></LeftNAv>
-          </Col>
-          <Col lg>
-             <h2>Main Content</h2>
-          </Col>
-          <Col lg>
-            <RightNav></RightNav>
-          </Col>
-        </Row>
-      </Container>
-      <Footer></Footer>
-    </div>
+        <Header></Header>
+        <h1>This is main</h1>
+        <Container>
+            <Row>
+            <Col lg>
+                <LeftNAv></LeftNAv>
+            </Col>
+            <Col lg>
+                <h2>Main Content</h2>
+            </Col>
+            <Col lg>
+                <RightNav></RightNav>
+            </Col>
+            </Row>
+        </Container>
+        <Footer></Footer>
+        </div>
   );
 };
 
